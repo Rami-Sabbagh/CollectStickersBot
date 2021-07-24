23a552f4acc03afcb12f9017662ad3551579f717
Rami Sabbagh <ramilego4game@gmail.com>
2021-07-24 22:27:30 +0300

    Create a statistics endpoint

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -7,8 +7,26 @@ const port = 3000;
 
 const redis = new Redis();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/statistics', async (_, res) => {
+    const stickers = await redis.hgetall('stickers_usage');
+
+    const rawCommands = await redis.hgetall('commands_usage');
+    const commands: Record<string, number> = {};
+
+    Object.entries(rawCommands).forEach(([key, value]) => commands[key] = Number.parseInt(value, 10));
+
+    const statistics = {
+        commands,
+
+        usersCount: await redis.scard('users') ?? 0,
+        stickers: {
+            static: Number.parseInt(stickers['static'] ?? '0', 10),
+            animated: Number.parseInt(stickers['animated'] ?? '0', 10),
+            image: Number.parseInt(stickers['image'] ?? '0', 10),
+        },
+    };
+
+    res.json(statistics);
 });
 
 async function main() {
